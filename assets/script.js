a3664d95601d66f168d6dfc2270ae49fc84d8416
Ali Tahir <alishan.tahir@gmail.com>
2016-07-13 14:55:47 -0400

    Update

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,27 @@
 
 
 
-var msg = 'Sign up to receive our newsletter for 10% off';
+// var msg = 'Sign up to receive our newsletter for 10% off';
 
-function updateMessage() {
+// function updateMessage() {
 
-	var el = document.getElementById('message');
+// 	var el = document.getElementById('message');
 
-	el.textContent = msg;
-}
+// 	el.textContent = msg;
+// }
 
-updateMessage();
+// updateMessage();
 
-function newMessage() {
+// function newMessage() {
 
-	document.getElementById('message1').innerHTML = '<h1><center><b>3 things coding</b></center></h1>'
-	document.getElementById('message2').innerHTML = '1. APplying'
-	document.getElementById('message3').innerHTML = '2. making'
-	document.getElementById('message4').innerHTML = '3. creating'
+// 	document.getElementById('message1').innerHTML = '<h1><center><b>3 things coding</b></center></h1>'
+// 	document.getElementById('message2').innerHTML = '1. APplying'
+// 	document.getElementById('message3').innerHTML = '2. making'
+// 	document.getElementById('message4').innerHTML = '3. creating'
 
-}
+// }
 
-newMessage();
+// newMessage();
 
 <!-- // Create a variable called el to hold the element whose id attribute has a value of info -->
 var el = document.getElementById('info');
@@ -29,3 +29,7 @@ var randomNum = Math.floor((Math.random() * 10) + 1);
 
 <!-- // Write the number into that element -->
 el.innerHTML = '<h2>random number</h2><p>' + randomNum + '</p>';
+
+var saying = 'Home sweet home';
+var message = '<h2>Lower:</h2><h1>' + saying[11] + '</h1>';
+var element = document.getElementById('messageInABottle').innerHTML = message;
